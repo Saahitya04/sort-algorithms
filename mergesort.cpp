9e65e9a1e6a124d148d309a2ad28591f0237fd76
Saahitya Bansal <saahityabansal1987@gmail.com>
2025-01-23 23:03:15 +0530

    commit merge sort

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,15 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//merge sort implementation
-vector<int> MergeSort(vector<int> arr) {
+void Merge(vector<int>& arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;
+    int right = mid+1;
+    while(left <= mid && right <= high) {
+        if(arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
 
+    for(int i = low; i <= high; i++) {
+        arr[i] = temp[i-low];
+    }
+}
+
+void MergeSort(vector<int>& arr, int low, int high) {
+    if(low >= high) return; 
+
+    int mid = (low+high)/2;
+    MergeSort(arr,low,mid);
+    MergeSort(arr,mid+1,high);
+    Merge(arr,low,mid,high);
 }
 
 int main() {
     vector<int> arr = {12,28,26,27,2,8,83,73,19};
-
-    arr = MergeSort(arr);
+    int n = arr.size();
+    MergeSort(arr,0,n-1);
     for(int i = 0; i < arr.size(); i++) {
         cout<<arr[i]<<" ";
     }
